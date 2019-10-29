ddb35eab6cb18731a8fa65d9a06d3517067eebff
henriquemsouza <henrique.m_s@hotmail.com>
2019-10-29 18:27:30 -0300

    sa

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
-
-server.all('*', function (req, res, next) {
-    if (req.method === 'GET') {
-      next() // Continue
-    } else {
-      res.sendStatus(403) // Forbidden
-    }
-  })
-
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {

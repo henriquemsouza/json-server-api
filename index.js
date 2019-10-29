ed51b42249c3934ca8655f9d8248d85d471639b2
henriquemsouza <henrique.m_s@hotmail.com>
2019-10-29 18:24:34 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,19 @@
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var port = Number(process.env.PORT || 3000);
+
+server.all('*', function (req, res, next) {
+    if (req.method === 'GET') {
+      next() // Continue
+    } else {
+      res.sendStatus(403) // Forbidden
+    }
+  })
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+    console.log('JSON Server is running')
+});
